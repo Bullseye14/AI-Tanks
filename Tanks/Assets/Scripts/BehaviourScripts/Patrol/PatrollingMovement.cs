ad5a7a7f174fdaa3f921b444381609915bb9fc66
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2020-11-02 18:37:21 +0100

    cleaning code

--- Tanks/Assets/Scripts/BehaviourScripts/Patrol/PatrollingMovement.cs
+++ Tanks/Assets/Scripts/BehaviourScripts/Patrol/PatrollingMovement.cs
@@ -9,15 +9,10 @@ public class PatrollingMovement : MonoBehaviour
     private NavMeshAgent agent;
     private int currentWaypoint;
     private bool isTravelling;
-    private bool isWaiting;
     private bool nextWaypoint = false;
-    private float waitTimer;
     private Vector3 target;
 
     // Public
-    public bool patrolWaiting; // if agents is waiting or not in a waypoint
-    public float totalWaitTime; // time that agent waits in each waypoint
-    //public float changeWaypointProbability = 0.2f; // probability to change direction
     public List<WayPoints> waypoints; // List of all waypoints in the game
 
     private void Awake()
@@ -42,28 +37,8 @@ public class PatrollingMovement : MonoBehaviour
         {
             isTravelling = false;
 
-            if (patrolWaiting)
-            {
-                isWaiting = true;
-                waitTimer = 0f;
-            }
-            else
-            {
-                ChangePatrolPoint();
-                SetDestination();
-            }
-        }
-
-        if (isWaiting)
-        {
-            waitTimer += Time.deltaTime;
-            if (waitTimer >= totalWaitTime)
-            {
-                isWaiting = false;
-
-                ChangePatrolPoint();
-                SetDestination();
-            }
+            ChangePatrolPoint();
+            SetDestination();  
         }
     }
 
@@ -79,9 +54,6 @@ public class PatrollingMovement : MonoBehaviour
 
     private void ChangePatrolPoint()
     {
-        /*if (UnityEngine.Random.Range(0f, 1f) <= changeWaypointProbability) ---> Probability to change patrolPoint
-            nextWaypoint = !nextWaypoint;*/
-
         if (nextWaypoint)
             currentWaypoint = (currentWaypoint + 1) % waypoints.Count;
         else
