6c6c47b5ab27b0bfb88556cf12ed17ab7745684d
xavimarin35 <36265010+xavimarin35@users.noreply.github.com>
2020-11-24 19:48:19 +0100

    update

--- Tanks/Assets/Scripts/Managers/MyGameManager.cs
+++ Tanks/Assets/Scripts/Managers/MyGameManager.cs
@@ -30,7 +30,7 @@ public class MyGameManager : MonoBehaviour
     public Text BWins_text;
     public Text RWins_text;
 
-    public TanksMoveManager TanksMove;
+    //public TanksMoveManager TanksMove;
     public FinalPanel finalPanel;
 
     void Start()
@@ -46,10 +46,10 @@ public class MyGameManager : MonoBehaviour
     {
         if (!redTank.activeSelf || !blueTank.activeSelf)
         {
-            AppearSpriteRed.SpriteRedInstance.visible1 = false;
-            AppearSpriteRed.SpriteRedInstance.visible2 = false;
-            AppearSpriteBlue.SpriteBlueInstance.visible1 = false;
-            AppearSpriteBlue.SpriteBlueInstance.visible2 = false;
+            //AppearSpriteRed.SpriteRedInstance.visible1 = false;
+            //AppearSpriteRed.SpriteRedInstance.visible2 = false;
+            //AppearSpriteBlue.SpriteBlueInstance.visible1 = false;
+            //AppearSpriteBlue.SpriteBlueInstance.visible2 = false;
 
             if (!gameFinished)
             {
@@ -58,14 +58,14 @@ public class MyGameManager : MonoBehaviour
                     blueWins += 1;
                     blueKills += 1;
                     redDeaths += 1;
-                    AppearSpriteBlue.SpriteBlueInstance.visible3 = true;
+                    //AppearSpriteBlue.SpriteBlueInstance.visible3 = true;
                 }
                 else if (redTank.activeSelf)
                 {
                     redWins += 1;
                     redKills += 1;
                     blueDeaths += 1;
-                    AppearSpriteRed.SpriteRedInstance.visible3 = true;
+                    //AppearSpriteRed.SpriteRedInstance.visible3 = true;
                 }
 
                 Timer.instance.timerActive = false;
@@ -92,8 +92,8 @@ public class MyGameManager : MonoBehaviour
         gameFinished = false;
         endTimer = 0;
 
-        AppearSpriteBlue.SpriteBlueInstance.visible3 = false;
-        AppearSpriteRed.SpriteRedInstance.visible3 = false;
+        //AppearSpriteBlue.SpriteBlueInstance.visible3 = false;
+        //AppearSpriteRed.SpriteRedInstance.visible3 = false;
 
         Timer.instance.timeStart = 0;
         Timer.instance.timerActive = true;
@@ -118,7 +118,7 @@ public class MyGameManager : MonoBehaviour
             {
                 tankHealth1.m_Dead = false; tankHealth2.m_Dead = false;
                 blueTank.SetActive(true); redTank.SetActive(true);
-                TanksMove.BPatrol = true; TanksMove.RWander = true;
+                //TanksMove.BPatrol = true; TanksMove.RWander = true;
 
                 tankHealth1.m_Slider.value = tankHealth1.m_CurrentHealth = tankHealth1.m_StartingHealth;
                 tankHealth2.m_Slider.value = tankHealth2.m_CurrentHealth = tankHealth2.m_StartingHealth;
