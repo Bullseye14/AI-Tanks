4c86e87eccdccbbc10d3d7921135ab5667fd13e1
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2020-12-05 11:24:08 +0100

    Starting conditions to pick drops

---
+++ Tanks/Assets/BehaviorBricks/Actions/System/MyActions/BluePickBullet.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Pada1.BBCore;
+using Pada1.BBCore.Tasks;
+using Pada1.BBCore.Framework;
+
+namespace BBUnity.Actions
+{
+    [Action("MyActions/BluePickBullet")]
+
+    public class BluePickBullet : GOAction
+    {
+        public UnityEngine.AI.NavMeshAgent navAgent;
+
+        [InParam("Blue Tank")]
+        public GameObject BlueTank;
+
+        public GameObject objective;
+
+        [InParam("audioSource")]
+        public AudioSource audioSource;
+
+        [InParam("bulletCharge")]
+        public AudioClip chargeSound;
+
+        public GameObject game;
+
+        private bool arrived = false;
+
+
+        public override void OnStart()
+        {
+            navAgent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+            BlueTank = GameObject.Find("Tank1");
+            objective = GameObject.Find("Bullet(Clone)");
+
+            arrived = false;
+
+            base.OnStart();
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            if (BlueTank.activeSelf)
+            {
+                if (!arrived)
+                {
+                    GoToBase();
+                }
+
+                else
+                {
+                    HeyHeyMate_GETSEMEMO();
+                }
+            }
+
+            return TaskStatus.RUNNING;
+        }
+        private void GoToBase()
+        {
+            Vector3 distance = Vector3.zero;
+
+            distance.x = Mathf.Abs(objective.transform.position.x - BlueTank.transform.position.x);
+            distance.z = Mathf.Abs(objective.transform.position.z - BlueTank.transform.position.z);
+
+            if (distance.x > 1 && distance.z > 1)
+                navAgent.SetDestination(objective.transform.position);
+
+            else
+                arrived = true;
+        }
+
+        private void HeyHeyMate_GETSEMEMO()
+        {
+            game = GameObject.Find("GameManager");
+
+            game.GetComponent<MyGameManager>().blueBullets += 1;
+
+            audioSource.clip = chargeSound;
+            audioSource.Play();
+
+            GameObject.Destroy(objective);
+        }
+    }
+}
+
---
+++ Tanks/Assets/BehaviorBricks/Conditions/System/MyConditions/Tank1/CanPickDropB.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+
+using Pada1.BBCore.Framework;
+using Pada1.BBCore;
+
+using UnityEngine.AI;
+
+[Condition("MyConditions/Tank1/Can blue pick bullet?")]
+[Help("Checks if the blue tank can pick bullet drop")]
+public class CanPickDropB : ConditionBase
+{
+    public GameObject game;
+    public GameObject bullet = null;
+
+    public GameObject self;
+    public GameObject enemy;
+    public override bool Check()
+    {
+        game = GameObject.Find("GameManager");
+
+        if (game.GetComponent<MyGameManager>().blueBullets < 3)
+        {
+            bullet = GameObject.Find("Bullet(Clone)");
+
+            if (bullet != null)
+            {
+                self = GameObject.Find("Tank1"); enemy = GameObject.Find("Tank2");
+
+                return true;
+                //return GoForAmmo(self.transform.position, enemy.transform.position, bullet.transform.position);
+            }
+            else return false;
+        }
+
+        else
+            return false;
+    }
+
+    private bool GoForAmmo(Vector3 s, Vector3 e, Vector3 b)
+    {
+        Vector3 s_e = Vector3.zero;
+        Vector3 s_b = Vector3.zero;
+        Vector3 e_b = Vector3.zero;
+
+        // Distance between Tanks
+        s_e.x = Mathf.Abs(e.x - s.x);
+        s_e.z = Mathf.Abs(e.z - s.z);
+
+        // Distance between Tank and Bullet
+        s_b.x = Mathf.Abs(b.x - s.x);
+        s_b.z = Mathf.Abs(b.z - s.z);
+
+        // Distance between Enemy and Bullet
+        e_b.x = Mathf.Abs(b.x - e.x);
+        e_b.z = Mathf.Abs(b.z - e.z);
+
+
+        if (s_e.x < s_b.x && s_e.z < s_b.z) // Tank is closest to Bullet than to Enemy
+        {
+            if (s_b.x < s_e.x && s_b.z < s_e.z) // Tank is closest to Bullet than the enemy
+            {
+                return true;
+            }
+            else return false; // Enemy is closest to Bullet than Tank
+        }
+        else return false; // Enemy is closest to Tank than to Bullet
+    }
+}
